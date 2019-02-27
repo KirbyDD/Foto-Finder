f6d0e70e347efa7165db5b3fc39f7ff6281fe063
DeMarcus Kirby <tkd024@gmail.com>
2019-02-27 03:57:05 -0700

    refactored all files

--- main.js
+++ main.js
@@ -9,203 +9,196 @@ var numOfFavs = document.querySelector('#fav-counter');
 var searchBox = document.querySelector('#search-box');
 var showToggleBtn = document.querySelector('#show-toggle');
 var cardArray = JSON.parse(localStorage.getItem('card')) || [];
-
 var newCard = new Photo();
 
 
 window.addEventListener('load', loadPreviousCards(cardArray));
 addBtn.addEventListener('click', convertImage);
-showToggleBtn.addEventListener('click', toggleCards)
-viewFavsBtn.addEventListener('click', toggleFavs)
+showToggleBtn.addEventListener('click', toggleCards);
+viewFavsBtn.addEventListener('click', toggleFavs);
 cardSection.addEventListener('click', deleteOrFav);
 cardSection.addEventListener('keyup', editContent);
 searchBox.addEventListener('keyup', function(e) {
-    if (searchBox.value) {
-      cardSection.innerHTML = '';
-      filterContent(searchBox.value);
-    } else if (!searchBox.value) {
+  if (searchBox.value) {
+    cardSection.innerHTML = '';
+    filterContent(searchBox.value);
+  } else if (!searchBox.value) {
       cardSection.innerHTML = '';
       loadPreviousCards();
-    }
-  });
+  }
+});
 
-function convertImage(e){
-    var reader = new FileReader();
-    var selectedImage = chooseFile.files[0];
-    reader.readAsDataURL(selectedImage);
-    reader.onload = function(){
-        imageResult(reader.result)
-    }
-    e.preventDefault();
+function convertImage(e) {
+  var reader = new FileReader();
+  var selectedImage = chooseFile.files[0];
+  reader.readAsDataURL(selectedImage);
+  reader.onload = function() {
+    imageResult(reader.result)
+  }
+  e.preventDefault();
 }
 
-function imageResult(newImage){
-    createCard(newImage);
+function imageResult(newImage) {
+  createCard(newImage);
 }
 
-function createCard(file){
-    var id = Math.floor(Date.now());
-    var title = titleInput.value;
-    var caption = captionInput.value;
-    if(cardSection.innerHTML == `<h2>Please add a photo</h2>`){
-        cardSection.innerHTML = '';
+function createCard(file) {
+  var id = Math.floor(Date.now());
+  var title = titleInput.value;
+  var caption = captionInput.value;
+  if(cardSection.innerHTML == `<h2>Please add a photo</h2>`) {
+    cardSection.innerHTML = '';
     }
-    if(!title || !caption || !file){
-        return;
-    } else {
-    newCard = new Photo(id, title, caption, file);
-    var cardArray = JSON.parse(localStorage.getItem('card'))
-    cardArray.push(newCard);
-    newCard.saveToStorage(cardArray);
-    cardSection.innerHTML = `<article data-id="${id}">
-    <p class="card-title card-p" contenteditable="true">${title}</p>
-    <img src="${file}" alt="" class="card-img">
-    <p class="card-body card-p" contenteditable="true">${caption}</p>
-    <div>
+  if(!title || !caption || !file) {
+    return;
+  } else {
+      newCard = new Photo(id, title, caption, file);
+      var cardArray = JSON.parse(localStorage.getItem('card'))
+      cardArray.push(newCard);
+      newCard.saveToStorage(cardArray);
+      cardSection.innerHTML = `<article data-id="${id}">
+      <p class="card-title card-p" contenteditable="true">${title}</p>
+      <img src="${file}" alt="" class="card-img">
+      <p class="card-body card-p" contenteditable="true">${caption}</p>
+      <div>
         <img src="images/delete.svg" alt="" class="delete-btn article-btn">
         <img src="images/favorite.svg" alt="" class="fav-btn article-btn">
-    </div>
-    </article>` + cardSection.innerHTML
-    }
+      </div>
+      </article>` + cardSection.innerHTML
+  }
 }
 
 function generateCards(id, title, caption, file, favorite) {
     var faveSrc = "images/favorite.svg";
-    if (favorite){
-        var faveSrc = 'images/favorite-active.svg'
+    if (favorite) {
+      var faveSrc = 'images/favorite-active.svg'
     }
-
     cardSection.innerHTML = `<article data-id="${id}">
     <p class="card-title card-p" contenteditable="true">${title}</p>
     <img src="${file}" alt="" class="card-img">
     <p class="card-body card-p" contenteditable="true">${caption}</p>
     <div>
-        <img src="images/delete.svg" alt="" class="delete-btn article-btn">
-        <img src=${faveSrc} alt="" class="fav-btn article-btn">
+      <img src="images/delete.svg" alt="" class="delete-btn article-btn">
+      <img src=${faveSrc} alt="" class="fav-btn article-btn">
     </div>
     </article>` + cardSection.innerHTML
 }
 
 
 function loadPreviousCards() {
-    if (cardArray.length > 10) {
-     var slicedCardArr = cardArray.slice(cardArray.length - 10);
-     cardSection.innerHTML = '';
-       for (var i = 0; i < slicedCardArr.length; i++) {
-        generateCards(slicedCardArr[i].id, slicedCardArr[i].title, slicedCardArr[i].caption,  slicedCardArr[i].file, cardArray[i].favorite);
-        setCount();
-      }
-    } else {
-        cardSection.innerHTML = '';
-        for (var i = 0; i < cardArray.length; i++) {
-        generateCards(cardArray[i].id, cardArray[i].title, cardArray[i].caption, cardArray[i].file, cardArray[i].favorite);
-        setCount();
+  if (cardArray.length > 10) {
+    var slicedCardArr = cardArray.slice(cardArray.length - 10);
+    cardSection.innerHTML = '';
+    for (var i = 0; i < slicedCardArr.length; i++) {
+    generateCards(slicedCardArr[i].id, slicedCardArr[i].title, slicedCardArr[i].caption,  slicedCardArr[i].file, cardArray[i].favorite);
     }
+  } else {
+      cardSection.innerHTML = '';
+      for (var i = 0; i < cardArray.length; i++) {
+      generateCards(cardArray[i].id, cardArray[i].title, cardArray[i].caption, cardArray[i].file, cardArray[i].favorite);
+    }
+    setCount();
     noPhotos();
-    // hideShowMore();
   }
 }
 
-function deleteOrFav(event){
-    (event.target.value ^= true)
-    if(event.target.classList.contains('delete-btn')) {
-    var articleId = event.target.parentElement.parentElement.dataset.id
-    newCard.deleteFromStorage(parseInt(articleId));
-        event.target.parentElement.parentElement.remove();
-        setCount();
-        noPhotos();
-    } else {
-        var articleId = event.target.parentElement.parentElement.dataset.id
-        swap(articleId);
-    }
+function deleteOrFav(event) {
+  (event.target.value ^= true)
+  if(event.target.classList.contains('delete-btn')) {
+  var articleId = event.target.parentElement.parentElement.dataset.id;
+  newCard.deleteFromStorage(parseInt(articleId));
+    event.target.parentElement.parentElement.remove();
+    setCount();
+    noPhotos();
+  } else {
+      var articleId = event.target.parentElement.parentElement.dataset.id;
+      toggleFaveBtn(articleId);
+  }
 }
 
-function swap(id){
-    if(event.target.classList.contains('fav-btn') && event.target.value == 1 ) {
-        event.target.src = 'images/favorite-active.svg';
-        newCard.updatePhoto(parseInt(id), true);
-        setCount();
-    } else if(event.target.classList.contains('fav-btn') && event.target.value == 0 ){
-        event.target.src = 'images/favorite.svg';
-        newCard.updatePhoto(parseInt(id), false);            
-        setCount();
-    }
+function toggleFaveBtn(id) {
+  if(event.target.classList.contains('fav-btn') && event.target.value == 1 ) {
+    event.target.src = 'images/favorite-active.svg';
+    newCard.updatePhoto(parseInt(id), true);
+    setCount();
+  } else if (event.target.classList.contains('fav-btn') && event.target.value == 0 ) {
+      event.target.src = 'images/favorite.svg';
+      newCard.updatePhoto(parseInt(id), false);            
+      setCount();
+  }
 }
 
-function editContent(event){
-    var articleId = event.target.parentElement.dataset.id;
-    var changeContent = event.target.innerText;
-    if (event.target.classList.contains('card-title')){
+function editContent(event) {
+  var articleId = event.target.parentElement.dataset.id;
+  var changeContent = event.target.innerText;
+  if (event.target.classList.contains('card-title')) {
     newCard.updateTitle(parseInt(articleId), changeContent);
-    } else {
-        newCard.updateCaption(parseInt(articleId), changeContent);
-    }
+  } else {
+      newCard.updateCaption(parseInt(articleId), changeContent);
+  }
 }
 
-function faveCount(){
-    let faveArray = JSON.parse(localStorage.getItem('card'));
-    let updatedArray = faveArray.filter(card => card.favorite);
-    return updatedArray.length;
+function faveCount() {
+  let faveArray = JSON.parse(localStorage.getItem('card'));
+  let updatedArray = faveArray.filter(card => card.favorite);
+  return updatedArray.length;
 }
 
-function setCount(){
-    console.log(numOfFavs);
-    var num = faveCount();
-    numOfFavs.innerText = num;
+function setCount() {
+  var num = faveCount();
+  numOfFavs.innerText = num;
 }
 
-function noPhotos(){
-    if(cardSection.innerHTML == ''){
-        cardSection.innerHTML = `<h2>Please add a photo</h2>`;
-    }
+function noPhotos() {
+  if(cardSection.innerHTML == '') {
+    cardSection.innerHTML = `<h2>Please add a photo</h2>`;
+  }
 }
 
-function filterContent(searchText){
-    var updatedCardArray = newCard.pullFromStorage();
-    var filterMatch = updatedCardArray.filter(card => card.title.toUpperCase().indexOf(searchText.toUpperCase()) === 0 ||
-      card.caption.toUpperCase().indexOf(searchText.toUpperCase()) === 0);
-    var filterMatchFav = updatedCardArray.filter(card => card.title.toUpperCase().indexOf(searchText.toUpperCase()) === 0 
-    && (card.favorite) ||
-    card.caption.toUpperCase().indexOf(searchText.toUpperCase()) === 0 && (card.favorite));
-    
-    if(filterMatchFav.length && viewFavsBtn.innerText == "View All Photos"){
-        for (var i = 0; i < filterMatchFav.length; i++) {
-            generateCards(filterMatchFav[i].id, filterMatchFav[i].title, filterMatchFav[i].caption, filterMatchFav[i].file, filterMatchFav[i].favorite);
+function filterContent(searchText) {
+  var updatedCardArray = newCard.pullFromStorage();
+  var filterMatch = updatedCardArray.filter(card => card.title.toUpperCase().indexOf(searchText.toUpperCase()) === 0 ||
+    card.caption.toUpperCase().indexOf(searchText.toUpperCase()) === 0);
+  var filterMatchFav = updatedCardArray.filter(card => card.title.toUpperCase().indexOf(searchText.toUpperCase()) === 0 
+    && (card.favorite) || card.caption.toUpperCase().indexOf(searchText.toUpperCase()) === 0 && (card.favorite));
+  if(filterMatchFav.length && viewFavsBtn.innerText == "View All Photos") {
+    for (var i = 0; i < filterMatchFav.length; i++) {
+      generateCards(filterMatchFav[i].id, filterMatchFav[i].title, filterMatchFav[i].caption, filterMatchFav[i].file, filterMatchFav[i].favorite);
         } 
-    } else if (filterMatch.length) {
-        for (var i = 0; i < filterMatch.length; i++) {
-          generateCards(filterMatch[i].id, filterMatch[i].title, filterMatch[i].caption, filterMatch[i].file, filterMatch[i].favorite);
-        } 
-    }
+  } else if (filterMatch.length) {
+      for (var i = 0; i < filterMatch.length; i++) {
+        generateCards(filterMatch[i].id, filterMatch[i].title, filterMatch[i].caption, filterMatch[i].file, filterMatch[i].favorite);
+      } 
+  }
 }
 
 function toggleCards() {
-    if (showToggleBtn.innerText == "Show More") {
-      showToggleBtn.innerText = "Show Less";
-      cardSection.innerHTML = '';
-      for (var i = 0; i < cardArray.length; i++) {
-        generateCards(cardArray[i].id, cardArray[i].title, cardArray[i].caption,  cardArray[i].file, cardArray[i].favorite);
-      }
-    } else {
+  if (showToggleBtn.innerText == "Show More") {
+    showToggleBtn.innerText = "Show Less";
+    cardSection.innerHTML = '';
+    for (var i = 0; i < cardArray.length; i++) {
+      generateCards(cardArray[i].id, cardArray[i].title, cardArray[i].caption,  cardArray[i].file, cardArray[i].favorite);
+    }
+  } else {
       showToggleBtn.innerText = "Show More";
       cardSection.innerHTML = '';
       loadPreviousCards();
-    }
   }
+}
 
-  function toggleFavs(event) {
-    event.preventDefault();
-    var updatedCardArray = newCard.pullFromStorage();
-    var filterMatch = updatedCardArray.filter(card => card.favorite)
-    if (viewFavsBtn.innerText !== "View All Photos") {
-      viewFavsBtn.innerText = "View All Photos";
-      cardSection.innerHTML = '';
-      for (var i = 0; i < filterMatch.length; i++) {
-        generateCards(filterMatch[i].id, filterMatch[i].title, filterMatch[i].caption,  filterMatch[i].file, filterMatch[i].favorite);
-      }
-    } else {
+function toggleFavs(event) {
+  event.preventDefault();
+  var updatedCardArray = newCard.pullFromStorage();
+  var filterMatch = updatedCardArray.filter(card => card.favorite)
+  if (viewFavsBtn.innerText !== "View All Photos") {
+    viewFavsBtn.innerText = "View All Photos";
+    cardSection.innerHTML = '';
+    for (var i = 0; i < filterMatch.length; i++) {
+      generateCards(filterMatch[i].id, filterMatch[i].title, filterMatch[i].caption,  filterMatch[i].file, filterMatch[i].favorite);
+    }
+  } else {
       viewFavsBtn.innerHTML = `View <span id="fav-counter">${faveCount()}</span> Favorites`;
       cardSection.innerHTML = '';
       loadPreviousCards();
-    }
   }
+}
--- photo.js
+++ photo.js
@@ -1,4 +1,4 @@
-class Photo{
+class Photo {
   constructor(id, title, caption, file, favorite) {
     this.id = id;
     this.title = title;
@@ -6,30 +6,23 @@ class Photo{
     this.file = file;
     this.favorite = false;
   }
-  favorite(){
-      if(this.favorite == true){
-          this.favorite = false;
-      } else {
-          this.favorite = true;
-      }
-  }
-  saveToStorage(array){
+  saveToStorage(array) {
     localStorage.setItem('card', JSON.stringify(array));
   }
   
-  deleteFromStorage(id){
+  deleteFromStorage(id) {
     var cardArray = this.pullFromStorage();
     cardArray.splice(this.getIndex(id), 1);
     this.saveToStorage(cardArray);
   }
 
-  updatePhoto(id, favorite){
+  updatePhoto(id, favorite) {
     var cardArray = this.pullFromStorage();
     cardArray[this.getIndex(id)].favorite = favorite;
     this.saveToStorage(cardArray);
   }
 
-  getIndex(id){
+  getIndex(id) {
     let storage = this.pullFromStorage();
     return storage.map(photo => photo.id).indexOf(id);
   }
@@ -38,13 +31,13 @@ class Photo{
     return JSON.parse(localStorage.getItem('card'));
   }
 
-  updateTitle(id, title){
+  updateTitle(id, title) {
     var cardArray = this.pullFromStorage();
     cardArray[this.getIndex(id)].title = title;
     this.saveToStorage(cardArray);
   }
 
-  updateCaption(id, caption){
+  updateCaption(id, caption) {
     var cardArray = this.pullFromStorage();
     cardArray[this.getIndex(id)].caption = caption;
     this.saveToStorage(cardArray);
